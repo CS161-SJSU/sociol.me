3bcc3749d567671fec3ebb4680bfc08834532272
Trinity Nguyen <thn.trinity@gmail.com>
2020-09-17 14:47:50 -0700

    Setup FE (#1)
    * :white_check_mark: Finish setting up FE codebase
    
    * finish setup

---
+++ client/pages/index.tsx
@@ -0,0 +1,17 @@
+import HelloWorld from '../components/HelloWorld'
+import styled from 'styled-components'
+
+const Page = styled.div`
+  margin-top: 10vh;
+  font-family: monospace;
+  text-align: center;
+`
+const Index = () => {
+  return (
+    <Page>
+      <HelloWorld />
+    </Page>
+  )
+}
+
+export default Index
