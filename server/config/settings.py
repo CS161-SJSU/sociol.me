789f12c17b5e85fe0ace369901e200691b152197
juliachin123 <juliachin8@gmail.com>
2020-10-10 19:45:39 -0700

    Delete space

--- server/config/settings.py
+++ server/config/settings.py
@@ -23,7 +23,6 @@ SECRET_KEY = env("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
