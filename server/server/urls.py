bd453ccb5c4fbeccbc1f637dc56e0dfe180a4321
toandaosjsu <toan.dao@sjsu.edu>
2020-10-13 14:12:56 -0700

    reset backend

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+ 
+urlpatterns = [ 
+    url(r'^', include('server.urls')),
 ]
