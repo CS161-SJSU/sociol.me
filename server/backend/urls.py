ca11c648a423649d1e8050d3abf765a8005dbdcb
toandaosjsu <toan.dao@sjsu.edu>
2020-10-20 23:30:36 -0700

    SSO hard code tokenId

--- server/backend/urls.py
+++ server/backend/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from backend import views 
  
 urlpatterns = [ 
-    url(r'^api/servers$', views.servers_list),
-    url(r'^api/servers/(?P<pk>[0-9]+)$', views.servers_detail),
-    url(r'^api/servers/published$', views.servers_list_published)
+    url(r'^$', views.servers_list),
+    url(r'^(?P<pk>[0-9]+)$', views.servers_detail),
+    url(r'^published$', views.servers_list_published),
+    url(r'^server$', views.to_servers)
 ]
--- server/backend/views.py
+++ server/backend/views.py
@@ -13,21 +13,10 @@ from rest_framework.permissions import IsAuthenticated  # <-- Here
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.response import Response
 
+from google.oauth2 import id_token
+from google.auth.transport import requests
+import requests as httprequest
 
-# class HelloView(APIView):
-    # permission_classes = (IsAuthenticated,)             # <-- And here
-    # def get(self, request):
-    #     content = {'message': 'Hello, World!'}
-    #     return Response(content)
-# @api_view(['GET'])
-# @authentication_classes([SessionAuthentication, BasicAuthentication])
-# @permission_classes([IsAuthenticated])
-# def example_view(request, format=None):
-#     content = {
-#         'user': unicode(request.user),  # `django.contrib.auth.User` instance.
-#         'auth': unicode(request.auth),  # None
-#     }
-#     return Response(content)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def servers_list(request):
@@ -86,4 +75,39 @@ def servers_list_published(request):
     if request.method == 'GET': 
         servers_serializer = SocialAppSerializer(servers, many=True)
         return JsonResponse(servers_serializer.data, safe=False)
+
+# SSO try out
+# https://developers.google.com/identity/sign-in/web/backend-auth
+
+@api_view(['POST','GET'])
+def to_servers(request):
+    servers = SocialApp.objects.filter(published=True)
     
+    if request.method == 'POST': 
+        print("if")
+        # token = request.data.get('tokenId')
+        token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjE3OGFiMWRjNTkxM2Q5MjlkMzdjMjNkY2FhOTYxODcyZjhkNzBiNjgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNDEzODg5MzE3OTYyLXU3cnJhNDI4Z2NtMmEzaW4xaWppNWppYWYxcjRzbnRjLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNDEzODg5MzE3OTYyLXU3cnJhNDI4Z2NtMmEzaW4xaWppNWppYWYxcjRzbnRjLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTEzOTkwOTc3NDgwMzIzODA1MjgyIiwiZW1haWwiOiJsZWVraW1idWk5MkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6Im5yT1ZuTnZzWmpDbTNkb296T2NQNnciLCJuYW1lIjoiTGVlIEtpbSIsInBpY3R1cmUiOiJodHRwczovL2xoNi5nb29nbGV1c2VyY29udGVudC5jb20vLTVvMHVUNGp4RkpnL0FBQUFBQUFBQUFJL0FBQUFBQUFBQUFBL0FNWnV1Y21PNWpxYkxVczVRRm5fTGw2T0NBc05YQ2ZHSXcvczk2LWMvcGhvdG8uanBnIiwiZ2l2ZW5fbmFtZSI6IkxlZSIsImZhbWlseV9uYW1lIjoiS2ltIiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2MDMyNjE1NDEsImV4cCI6MTYwMzI2NTE0MSwianRpIjoiZDg5MTNlZjc4MDAwODZjZDI5M2E4MTY5N2Q1MmE3YjFhYWUxYjMxMiJ9.W4aIYZ5X_OqFoEku4VmYzT3EiEWfYajPZVLV0gGRtxZAmisoCeOYF3uOPoWFSmEWnBaOFruN98utbMZxNW_gd-KJQDNOqPwciFsTm3XgEZ5fY1VNfTB1_r3jZHzEb4S6Hm-nulT_RWUrUme9WxcZkfFmgZM396d1gL-b0Xx1buFIoqJ8LPDb8dBzShtSiIv3woHw16bTL0wKYcd5dCpue6VeKaY0agTP_LEZT58vZQVQbaImnIWDz2TGmbbLkTCqu_5MmsJrU0LXq-1k4M9Uv8WkgGt_ADWRVw9aV6TLGCLfWkNsGcbbvmyqsts7HsD0loShiP-AhKJB1QU1gvV53g"
+
+        try:
+            print("try")
+            CLIENT_ID = "413889317962-u7rra428gcm2a3in1iji5jiaf1r4sntc.apps.googleusercontent.com"
+
+            idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+            print(idinfo)
+
+            # ID token is valid. Get the user's Google Account ID from the decoded token.
+            userid = idinfo['sub']
+    
+            response = httprequest.get("https://oauth2.googleapis.com/tokeninfo?id_token=" + token)
+            print(response.json())
+
+            return Response({'message': 'Google idinfo OK!'}, status=status.HTTP_202_ACCEPTED)
+
+        except ValueError:
+            # Invalid token
+            print("hehehe")
+            return Response({'message': 'Google idinfo is wrong!'})
+    
+
+    print("hohoho")
+    return Response({'message': 'Google try!'})
