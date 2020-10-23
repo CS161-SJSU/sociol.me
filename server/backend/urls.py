0ad67b0f8b137cd7dbeb94837ec039212c9b9bc2
toandaosjsu <toan.dao@sjsu.edu>
2020-10-23 00:49:33 -0700

    data collected and displayed, not save to DB yet

--- server/backend/urls.py
+++ server/backend/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # url(r'^$', views.servers_list),
     # url(r'^(?P<pk>[0-9]+)$', views.servers_detail),
     # url(r'^published$', views.servers_list_published),
-    url(r'^server$', views.to_servers)
+    url(r'^googleToken$', views.to_servers)
 ]
