284814343d35d5a00fc1076183efb89847cd79f6
Trinity Nguyen <thn.trinity@gmail.com>
2020-11-01 17:03:31 -0800

    Finish Landing Page

--- client/components/sections/Cta.js
+++ client/components/sections/Cta.js
@@ -60,7 +60,7 @@ const Cta = ({
               <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
-                  fill="#a2d9ff"
+                  fill="#376DF9"
                 />
               </svg>
             </Input>
