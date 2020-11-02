2f607919573d0886dae8d24dc996bfac640a9859
Eric Wu <wu.eric123@gmail.com>
2020-11-01 21:24:01 -0800

    fixed model type

--- server/twitter/models.py
+++ server/twitter/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class TwitterModel(models.Model):
     email = models.ForeignKey(max_length=70, blank=False, default='')
     name = models.CharField(max_length=70, blank=False, default='')
-    id = models.CharField(max_length=70, blank=False, default='')
+    id = models.IntegerField(max_length=70, blank=False, default='')
     screen_name = models.CharField(max_length=70, blank=False, default='')
     description = models.CharField(max_length=1000, blank=False, default='')
-    followers_count =  models.CharField(max_length=70, blank=False, default='')
-    friends_count = models.CharField(max_length=70, blank=False, default='')
+    followers_count =  models.IntegerField(max_length=70, blank=False, default='')
+    friends_count = models.IntegerField(max_length=70, blank=False, default='')
     auth_token =  models.CharField(max_length=2000, blank=False, default='')
     auth_token_secret = models.CharField(max_length=2000, blank=False, default='')
