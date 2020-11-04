bc0bb25c7b708ce7dc5c07ce7ab7e9de6ae9e6a9
Trinity Nguyen <thn.trinity@gmail.com>
2020-11-04 13:48:28 -0800

    Redesign Login  (#17)
    * Redesign login
    
    * Minor fix

---
+++ client/components/FormPage.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+type Props = {
+  children?: React.Node
+  imageURL?: string
+}
+
+const Wrapper = styled.div`
+  height: 100%;
+  margin: 0;
+  padding: 0;
+`
+const Page = styled.div`
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -ms-flex-pack: center;
+  justify-content: center;
+  height: 68rem;
+`
+
+function FormPage(props: Props): React.Node {
+  return (
+    <Wrapper>
+      <Page>
+        <div className="page-single">
+          <div className="container">
+            <div className="row">
+              <div className="col col-login mx-auto">
+                <div className="center-content mb-6">
+                  <img src={props.imageURL} className="h-7" alt="logo" />
+                </div>
+                {props.children}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Page>
+    </Wrapper>
+  )
+}
+
+export default FormPage
