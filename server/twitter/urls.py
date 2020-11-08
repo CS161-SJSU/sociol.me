bd632ed8cb3ba93dd5ed0128ea745c97ddeaa9df
Toan Dao <42660105+toandaosjsu@users.noreply.github.com>
2020-11-08 12:58:26 -0800

    Twitter 5 top and worst tweet API (#25)

--- server/twitter/urls.py
+++ server/twitter/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^twitter/auth/', views.twitter_authenticate),
     url(r'^twitter/verify/$', views.verify),
     url(r'^twitter/info/', views.get_twitter_info),
+    url(r'^twitter/topworst/$', views.topworst),
 ]
